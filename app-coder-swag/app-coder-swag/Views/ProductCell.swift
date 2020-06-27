9447dc2df1dede50d859b4eb32d5bc3843f710d0
mailys <mailys.perez@epitech.eu>
2020-06-27 12:26:48 +0200

    [ADD] design UICollectionViewCell

---
+++ app-coder-swag/app-coder-swag/Views/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  app-coder-swag
+//
+//  Created by Maïlys Perez on 27/06/2020.
+//  Copyright © 2020 Maïlys Perez. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
